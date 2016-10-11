575129ae21a882c4dba1f34a768c7e65b668ef4d
Liber Dovat <liber.dovat@gmail.com>
2016-10-11 16:45:32 -0300

    leo los archivos binarios

--- PRSfr/cargar_imgs.py
+++ PRSfr/cargar_imgs.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 
+import struct
+import numpy
+
 # RUTAsat = '/sat/prd-sat/ART_G015x015GG_C015x015/'
 # PATHfr  = RUTAsat + 'B01-FR/2016/ART_2016275_143500.FR'
 
@@ -24,11 +27,22 @@ Cj = meta(2);
 Ct = Ci*Cj;
 '''
 
-meta       = open(RUTAsat + 'meta/T000gri.META',   'r')
-# LATdeg_vec = open(RUTAsat + 'meta/T000gri.LATvec', 'r')
-# LONdeg_vec = open(RUTAsat + 'meta/T000gri.LONvec', 'r')
+fid = open(RUTAsat + 'meta/T000gri.META', 'rb')
+meta = numpy.fromfile(fid, dtype='float32')
+fid.close()
 
-ba = bytearray(meta.read())
-for byte in ba:
-    print byte & 1
-    
+fid = open(RUTAsat + 'meta/T000gri.LATvec', 'r')
+LATdeg_vec = numpy.fromfile(fid, dtype='float32')
+fid.close()
+
+fid = open(RUTAsat + 'meta/T000gri.LONvec', 'r')
+LONdeg_vec = numpy.fromfile(fid, dtype='float32')
+fid.close()
+
+print meta
+print LATdeg_vec
+print LONdeg_vec
+
+print meta.size
+print LATdeg_vec.size
+print LATdeg_vec.size
