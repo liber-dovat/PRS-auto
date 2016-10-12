0407df1ab95639d00cc05ec715209cfa7f502b34
Liber Dovat <liber.dovat@gmail.com>
2016-10-12 20:41:43 -0300

    seteados el colorbar y un nombre genérico de pie de imagen

--- PRSfr/cargar_imgs.py
+++ PRSfr/cargar_imgs.py
@@ -3,74 +3,79 @@
 import matplotlib
 matplotlib.use('Agg')
 
-from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import struct
 import numpy
+import os
+
+from os.path              import basename
 
 # RUTAsat = '/sat/prd-sat/ART_G015x015GG_C015x015/'
 # PATHfr  = RUTAsat + 'B01-FR/2016/ART_2016275_143500.FR'
 
 # print PATHfr
 
-RUTAsat = './test_fr/'
-PATHpng = RUTAsat + 'png/'
-PATHfr  = RUTAsat + 'B01-FR/2016/ART_2016285_133500.FR'
+def frtopng(metaPath, file):
 
-fid = open(RUTAsat + 'meta/T000gri.META', 'r')
-meta = numpy.fromfile(fid, dtype='float32')
-fid.close()
+  fid = open(metaPath + '/T000gri.META', 'r')
+  meta = numpy.fromfile(fid, dtype='float32')
+  fid.close()
 
-fid = open(RUTAsat + 'meta/T000gri.LATvec', 'r')
-LATdeg_vec = numpy.fromfile(fid, dtype='float32')
-LATdeg_vec = LATdeg_vec[::-1] # invierto el arreglo porque quedaba invertido verticalmente
-fid.close()
+  fid = open(metaPath + '/T000gri.LATvec', 'r')
+  LATdeg_vec = numpy.fromfile(fid, dtype='float32')
+  LATdeg_vec = LATdeg_vec[::-1] # invierto el arreglo porque quedaba invertido verticalmente
+  fid.close()
 
-fid = open(RUTAsat + 'meta/T000gri.LONvec', 'r')
-LONdeg_vec = numpy.fromfile(fid, dtype='float32')
-fid.close()
+  fid = open(metaPath + '/T000gri.LONvec', 'r')
+  LONdeg_vec = numpy.fromfile(fid, dtype='float32')
+  fid.close()
 
-# obtengo del vector meta el largo y alto de elementos de los vectores y los datos
-Ci = meta[0];
-Cj = meta[1];
-Ct = Ci*Cj;
+  # obtengo del vector meta el largo y alto de elementos de los vectores y los datos
+  Ci = meta[0];
+  Cj = meta[1];
+  Ct = Ci*Cj;
 
-print "Lon min:" + str(numpy.amin(LONdeg_vec)) + ", Lon max:" + str(numpy.amax(LONdeg_vec))
-print "Lat min:" + str(numpy.amin(LATdeg_vec)) + ", Lat max:" + str(numpy.amax(LATdeg_vec))
+  print "Lon min:" + str(numpy.amin(LONdeg_vec)) + ", Lon max:" + str(numpy.amax(LONdeg_vec))
+  print "Lat min:" + str(numpy.amin(LATdeg_vec)) + ", Lat max:" + str(numpy.amax(LATdeg_vec))
 
-axes = plt.gca()
-axes.set_xlim([numpy.amin(LONdeg_vec),numpy.amax(LONdeg_vec)])
-axes.set_ylim([numpy.amin(LATdeg_vec),numpy.amax(LATdeg_vec)])
+  # plt.axis('scaled')
 
-# imagen
-fid = open(PATHfr, 'r')
-data = numpy.fromfile(fid, dtype='float32')
-fid.close()
-data[data == 0.0] = numpy.nan
-IMG1 = numpy.reshape(data, (Ci, Cj))
-print IMG1.shape
+  # seteo los minimos y maximos de la imagen en funcion de los min y max de lat y long
+  axes = plt.gca()
+  axes.set_xlim([numpy.amin(LONdeg_vec),numpy.amax(LONdeg_vec)])
+  axes.set_ylim([numpy.amin(LATdeg_vec),numpy.amax(LATdeg_vec)])
 
-cs = plt.pcolormesh(LONdeg_vec, LATdeg_vec, IMG1, cmap='jet')
+  # abro el archivo FR
+  fid = open(file, 'r')
+  data = numpy.fromfile(fid, dtype='float32')
+  fid.close()
+  IMG1 = numpy.reshape(data, (Ci, Cj))
+  print IMG1.shape
 
-# agrego el colorbar
-cbar = plt.colorbar(cs)
+  # grafico IMG1 usando lon como vector x y lat como vector y
+  cs = plt.pcolormesh(LONdeg_vec, LATdeg_vec, IMG1, cmap='jet')
 
-# agrego el logo en el documento
-logo = plt.imread('/sat/PRS/libs/PRS-auto/PRSpng/imgs/les-logo.png')
-plt.figimage(logo, 5, 5)
+  # dado que FR y RP van de 0 a 100 seteo esos rangos para el colorbar
+  plt.clim(0,100)
 
-plt.annotate("tag", (0,0), (140, -50), xycoords='axes fraction', textcoords='offset points', va='top', fontsize=10, family='monospace')
+  # agrego el colorbar
+  cbar = plt.colorbar(cs, ticks=[0., 20., 40., 60., 80., 100.])
+  cbar.ax.set_xticklabels([0., 20., 40., 60., 80., 100.], fontsize=10)
 
-plt.savefig(PATHpng + 'file.png', bbox_inches='tight', dpi=200)
-plt.close()
+  # agrego el logo en el documento
+  logo = plt.imread('/sat/PRS/libs/PRS-auto/PRSpng/imgs/les-logo.png')
+  plt.figimage(logo, 5, 5)
 
-# % --- imagen
-# fid = fopen(PATHfr, 'r');
-# data = fread(fid, Ct, 'float32');
-# fclose(fid);
-# IMG1 = reshape(data, Cj, Ci);
+  name = basename(file)
 
-# figure(1)
-# imagesc(LONdeg_vec, LATdeg_vec, flipud(IMG1'));
-# view(2); shading interp; axis tight; axis xy;
-# colorbar;
+  # genero el pie de la imagen, con el logo y la info del arcivo
+  plt.annotate(name, (0,0), (140, -25), xycoords='axes fraction', textcoords='offset points', va='top', fontsize=12, family='monospace')
+
+  PATHpng = './test_fr/png/'
+  plt.savefig(PATHpng + name +'.png', bbox_inches='tight', dpi=200)
+  plt.close() # cierro el archivo
+
+# frtopng
+
+frtopng('./test_fr/meta/', './test_fr/B01-FR/2016/ART_2016285_133500.FR')
+frtopng('./test_fr/meta/', './test_fr/B01-RP/2016/ART_2016285_133500.RP')
