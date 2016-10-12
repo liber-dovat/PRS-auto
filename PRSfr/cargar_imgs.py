8843e23e9e0a27e21cdeef82a4dc997b755ee1f3
Liber Dovat <liber.dovat@gmail.com>
2016-10-12 18:59:39 -0300

    puedo dibujar una imágen. hay que ver si todos los elementos están correctos

--- PRSfr/cargar_imgs.py
+++ PRSfr/cargar_imgs.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python
 
+import matplotlib
+matplotlib.use('Agg')
+
+from mpl_toolkits.basemap import Basemap
+import matplotlib.pyplot as plt
 import struct
 import numpy
 
@@ -9,6 +14,8 @@ import numpy
 # print PATHfr
 
 RUTAsat = './test_fr/'
+PATHpng = RUTAsat + 'png/'
+PATHfr  = RUTAsat + 'B01-FR/2016/ART_2016285_133500.FR'
 
 '''
 % --- Cargar metadatos
@@ -27,12 +34,15 @@ Cj = meta(2);
 Ct = Ci*Cj;
 '''
 
+numpy.set_printoptions(suppress=True)
+
 fid = open(RUTAsat + 'meta/T000gri.META', 'r')
 meta = numpy.fromfile(fid, dtype='float32')
 fid.close()
 
 fid = open(RUTAsat + 'meta/T000gri.LATvec', 'r')
 LATdeg_vec = numpy.fromfile(fid, dtype='float32')
+LATdeg_vec = LATdeg_vec[::-1]
 fid.close()
 
 fid = open(RUTAsat + 'meta/T000gri.LONvec', 'r')
@@ -52,4 +62,29 @@ print meta.size
 print LATdeg_vec.size
 print LATdeg_vec.size
 
+print Ci
+print Cj
 print Ct
+
+# imagen
+fid = open(PATHfr, 'r')
+data = numpy.fromfile(fid, dtype='float32')
+fid.close()
+IMG1 = numpy.reshape(data, (Ci, Cj))
+print IMG1.shape
+
+plt.pcolormesh(LONdeg_vec, LATdeg_vec, IMG1, cmap='jet')
+
+plt.savefig(PATHpng + 'file.png', bbox_inches='tight', dpi=200)
+plt.close()
+
+# % --- imagen
+# fid = fopen(PATHfr, 'r');
+# data = fread(fid, Ct, 'float32');
+# fclose(fid);
+# IMG1 = reshape(data, Cj, Ci);
+
+# figure(1)
+# imagesc(LONdeg_vec, LATdeg_vec, flipud(IMG1'));
+# view(2); shading interp; axis tight; axis xy;
+# colorbar;
