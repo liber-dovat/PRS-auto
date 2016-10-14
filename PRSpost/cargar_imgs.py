58c5551485b4cd52db8f930f74fd1cd23fb507d3
Liber Dovat <liber.dovat@gmail.com>
2016-10-14 17:25:44 -0300

    limpiando el codigo. CON ERROR

--- PRSpost/cargar_imgs.py
+++ PRSpost/cargar_imgs.py
@@ -18,22 +18,22 @@ from mpl_toolkits.basemap import Basemap
 
 # print PATHfr
 
-def rangoColorbar(band):
+def rangoColorbar(ext):
 
   # defino los rangos del colorbar en funcion del tipo de banda
-  if band == 'FR' or band == 'RP':
+  if ext == 'FR' or ext == 'RP':
     vmin = 0.
     vmax = 100.
-  elif band == 'T2':
+  elif ext == 'T2':
     vmin = -68.
     vmax = 47. 
-  elif band == 'T3':
+  elif ext == 'T3':
     vmin = -68.
     vmax = -8.
-  elif band == 'T4':
+  elif ext == 'T4':
     vmin = -80.
     vmax = 50.
-  elif band == 'T6':
+  elif ext == 'T6':
     vmin = -68.
     vmax = 7.
 
@@ -98,18 +98,15 @@ def nameTag(basename):
 
 def frtopng(metaPath, file):
 
-  # abro el archivo meta y guardo los datos
   fid = open(metaPath + '/T000gri.META', 'r')
   meta = numpy.fromfile(fid, dtype='float32')
   fid.close()
 
-  # abro el archivo T000gri.LATvec y guardo los datos
   fid = open(metaPath + '/T000gri.LATvec', 'r')
   LATdeg_vec = numpy.fromfile(fid, dtype='float32')
   LATdeg_vec = LATdeg_vec[::-1] # invierto el arreglo porque quedaba invertido verticalmente
   fid.close()
 
-  # abro el archivo T000gri.LONvec y guardo los datos
   fid = open(metaPath + '/T000gri.LONvec', 'r')
   LONdeg_vec = numpy.fromfile(fid, dtype='float32')
   fid.close()
@@ -119,10 +116,9 @@ def frtopng(metaPath, file):
   Cj = meta[1];
   Ct = Ci*Cj;
 
-  # con esto quito el formato de escritura de numeracion cientifica
-  # numpy.set_printoptions(suppress=True)
+  numpy.set_printoptions(suppress=True)
 
-  # print file
+  print file
   # print meta
   # print Ci
   # print Cj
@@ -130,69 +126,62 @@ def frtopng(metaPath, file):
   # print LATdeg_vec.size
   # print LATdeg_vec.size
 
-  min_lon = numpy.amin(LONdeg_vec)
-  max_lon = numpy.amax(LONdeg_vec)
-  min_lat = numpy.amin(LATdeg_vec)
-  max_lat = numpy.amax(LATdeg_vec)
+  print "Lon min:" + str(numpy.amin(LONdeg_vec)) + ", Lon max:" + str(numpy.amax(LONdeg_vec))
+  print "Lat min:" + str(numpy.amin(LATdeg_vec)) + ", Lat max:" + str(numpy.amax(LATdeg_vec))
 
-  print "Lon min:" + str(min_lon) + ", Lon max:" + str(max_lon)
-  print "Lat min:" + str(min_lat) + ", Lat max:" + str(max_lat)
+  # plt.axis('scaled')
 
   # seteo los minimos y maximos de la imagen en funcion de los min y max de lat y long
   axes = plt.gca()
-  axes.set_xlim([min_lon, max_lon])
-  axes.set_ylim([min_lat, max_lat])
+  axes.set_xlim([numpy.amin(LONdeg_vec),numpy.amax(LONdeg_vec)])
+  axes.set_ylim([numpy.amin(LATdeg_vec),numpy.amax(LATdeg_vec)])
 
-  # abro el archivo file y lo guardo en data
+  # abro el archivo FR
   fid = open(file, 'r')
   data = numpy.fromfile(fid, dtype='float32')
   fid.close()
-
-  # paso el vector data a una matriz de tamano Ci Cj
+  # print data.shape
   IMG = numpy.reshape(data, (Ci, Cj))
+  # print IMG.shape
 
-  # genero un mapa con la proyecccion de mercator y lat y lons los anteriores
   ax1 = Basemap(projection='merc',\
-                llcrnrlat=min_lat,urcrnrlat=max_lat,\
-                llcrnrlon=min_lon,urcrnrlon=max_lon,\
+                llcrnrlat=numpy.amin(LATdeg_vec),urcrnrlat=numpy.amax(LATdeg_vec),\
+                llcrnrlon=numpy.amin(LONdeg_vec),urcrnrlon=numpy.amax(LONdeg_vec),\
                 resolution='l')
 
-  # dibujo las costas, estados y paises
   ax1.drawcoastlines()
   ax1.drawstates()
   ax1.drawcountries()
 
-  # dibujo los valores de latitudes y longitudes al margen de la imagen
+  # dibujo los valores de latitudes y longitudes
   ax1.drawparallels(numpy.arange(-45, -20, 5), labels=[1,0,0,0], linewidth=0.0, fontsize=10)
   ax1.drawmeridians(numpy.arange(-70, -45, 5), labels=[0,0,1,0], linewidth=0.0, fontsize=10)
 
-  # genero un meshgrid a partir de LonVec y LatVec
   lons2d, lats2d = numpy.meshgrid(LONdeg_vec, LATdeg_vec)
-  # y luego obtengo sus coordenadas en el mapa ax
   x, y = ax1(lons2d,lats2d)
 
-  # obtengo la extension del archivo para mapear la banda
-  band = getExt(file)
+  ext = getExt(file)
 
-  # defino el min y max en funcion de la banda
-  vmin, vmax = rangoColorbar(band)
-
-  if band == 'FR' or band == 'RP':
-    cmap='jet'
-    plt.clim(vmin, vmax)
-    ticks=[0., 20., 40., 60., 80., 100.]
-  else:
-    # Los datos de T2 a T6 estan en kelvin, asi que los paso a Celsius
-    IMG -= 273.
-    cmap = _get_inumet(1024)
-    ticks=[vmin, 0., vmax]
-  # if FR o RP
+  vmin, vmax = rangoColorbar(ext)
 
   # grafico IMG1 usando lon como vector x y lat como vector y
-  cs = ax1.pcolormesh(x, y, IMG, vmin=vmin, vmax=vmax, cmap=cmap)
-
-  if band == 'FR' or band == 'RP':
+  # dado que FR y RP van de 0 a 100 seteo esos rangos para el colorbar
+  if ext == 'FR' or ext == 'RP':
+    cs = ax1.pcolormesh(x, y, IMG, vmin=vmin, vmax=vmax, cmap='jet')
     plt.clim(vmin, vmax)
+
+    ticks=[0., 20., 40., 60., 80., 100.]
+  else:
+
+    # Los datos de T2 a T6 estan en kelvin, asi que los paso a Celsius
+    IMG -= 273.
+
+    inumet = _get_inumet(1024)
+    cs = ax1.pcolormesh(x, y, IMG, vmin=vmin, vmax=vmax, cmap=inumet)
+
+    ticks=[vmin, 0., vmax]
+
+  # if FR o RP
 
   # agrego el colorbar
   cbar = ax1.colorbar(cs, location='bottom', pad='3%', ticks=ticks)
@@ -205,7 +194,7 @@ def frtopng(metaPath, file):
   # genero los datos para escribir el pie de pagina
   name     = basename(file)           # obtengo el nombre base del archivo
   PATHpng = './test/png/'
-  destFile = PATHpng + band + name +'.png' # determino el nombre del archivo a escribir
+  destFile = PATHpng + ext + name +'.png' # determino el nombre del archivo a escribir
 
   tag = nameTag(name)
 
