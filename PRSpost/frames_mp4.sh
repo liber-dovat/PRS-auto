d63ba77bd620a9f9e2789afb3e4a3052434a2fe5
Liber Dovat <liber.dovat@gmail.com>
2016-11-01 19:08:36 -0300

    agregado el cd en el script para que genere los archivos en las carpetas correctas

--- PRSpost/frames_mp4.sh
+++ PRSpost/frames_mp4.sh
@@ -4,5 +4,5 @@ BANDA=$2
 INDIR=$1
 OUTDIR="/sat/prd-sat/PNGs/"
 
-convert -crop 948x1132+0+0 +repage $INDIR*.png $INDIRrepage%03d.png
-ffmpeg -framerate 5 -i $INDIRrepage%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p $OUTDIR$BANDA.mp4
+cd $INDIR; convert -crop 948x1132+0+0 +repage *.png repage%03d.png
+cd $INDIR; ffmpeg -y -framerate 5 -i repage%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p $OUTDIR$BANDA.mp4
