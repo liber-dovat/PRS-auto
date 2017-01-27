13f80c292bff5b2e85ad3983794f2314a0eeac41
Liber Dovat <liber.dovat@gmail.com>
2017-01-27 15:30:25 -0300

    corregido el color de fondo de los videos

--- PRSpost/frames_mp4.sh
+++ PRSpost/frames_mp4.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# INDIR: Directorio donde se encuentras los archivos png
+# BANDA: Nombre de la banda, y se utiliza para crear el nombre del archivo
+
 BANDA=$2
 INDIR=$1
 OUTDIR="/sat/prd-sat/PNGs/"
 
 echo "genero el repage";
-cd $INDIR; convert -crop 950x1160+0+0 +repage ART*.png repage%03d.png;
+cd $INDIR; convert -background "#4F7293" -alpha remove -crop 950x1170+0+0 +repage ART*.png repage%03d.png;
 echo "genero el video en mp4";
 cd $INDIR; ffmpeg -y -framerate 3 -i repage%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p $OUTDIR$BANDA.mp4;
 # echo "genero el video en webm";
